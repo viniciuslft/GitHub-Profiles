b704b8b9d16b2cabbcdb634fbac6a722fc972bef
Vinicius Pedroza <viniciuspedrozaestrela@gmail.com>
2024-07-11 16:24:40 -0300

    script Vue 3 adicionado com busca de usuário + token pessoal (se necessário)

---
+++ script.js
@@ -0,0 +1,30 @@
+const GITHUB_TOKEN = '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';
+
+const app = Vue.createApp({
+    data() {
+        return {
+            showSearch: true,
+            username: '',
+            user: {},
+        };
+    },
+    methods: {
+        fetchUser() {
+            axios.get(`https://api.github.com/users/${this.username}`, {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`
+                }
+            })
+                .then(response => {
+                    this.user = response.data;
+                    this.showSearch = false;
+                })
+                .catch(error => {
+                    alert('Usuário não encontrado. Tente novamente.');
+                    console.error('Erro:', error);
+                });
+        }
+    }
+});
+
+app.mount('#app');
